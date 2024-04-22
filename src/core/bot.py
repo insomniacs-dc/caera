bacb09ca4d6792dee667c28b82fb91a2d4205076
externref <shiva02939@gmail.com>
2024-04-22 11:12:53 +0530

    base setup and commands

--- src/core/bot.py
+++ src/core/bot.py
@@ -1,15 +1,41 @@
+import logging
 import os
 
 import disnake
 from disnake.ext import commands
 
+from src.core.setups import Cog, create_logging_setup
+
+main_logger = logging.getLogger("caera")
+
+create_logging_setup(main_logger)
+
 
 class Caera(commands.Bot):
+    logger = main_logger
+    version = "0.0.1a"
+
     def __init__(self) -> None:
-        super().__init__(command_prefix="c!", intents=disnake.Intents.all())
+        command_sync_flags = commands.CommandSyncFlags.default()
+        command_sync_flags.sync_commands_debug = True
+        super().__init__(
+            command_prefix="c!",
+            intents=disnake.Intents.all(),
+            command_sync_flags=command_sync_flags,
+        )
         self.load_extension("jishaku")
 
-    async def setup(self) -> None: ...
+    async def on_ready(self) -> None:
+        self.logger.info("Bot is online! ✅")
+
+    async def setup(self) -> None:
+        for ext in filter(
+            lambda f: f.endswith(".py") and not f.startswith("_s"),
+            os.listdir("src/exts"),
+        ):
+            self.logger.info(f"Loading {ext} ⭕")
+            self.load_extension(f"src.exts.{ext.replace('.py','')}")
+            self.logger.info(f"Loaded {ext} ✅")
 
     async def start(self) -> None:
         await self.setup()
